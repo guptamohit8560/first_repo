35e83b83af1d691b64d78931165809078c51f456
mohit gupta <guptamohit8560@gmail.com>
2022-04-05 15:32:42 +0530

    save the first code

---
+++ classes.py
@@ -0,0 +1,19 @@
+class book():
+	def __init__(self,bname,byear,bauthor):
+		self.bname= bname
+		self.byear=byear
+		self.bauthor= bauthor
+	def b_name(self):
+		self.bname= self.bname + 'mohit'
+
+
+a= book('ASOIAF',2012,'GRRM')
+
+a.publish=2018
+
+print (a.__dict__)
+
+a.b_name()
+
+print (a.__dict__)
+
